a4abcacfce2ef9ac8731611f57cedd23a3e1ca92
Emil Balicki <emil.balicki2@gmail.com>
2023-05-12 21:35:52 +0200

    Added a comment

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,4 +1,6 @@
 module.exports = {
   // URL of the MongoDB database to connect to.
-  url: "mongodb://mongodb:27017/datastore", // Here: mongodb://<IP>:<PORT>/<COLLECTION_NAME>
+  // With Docker, the name of the container can also be provided instead of the IP address.
+  // Here: mongodb://<IP>:<PORT>/<COLLECTION_NAME>
+  url: "mongodb://mongodb:27017/datastore",
 };
