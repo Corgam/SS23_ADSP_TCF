d3e23c3a5ea3b8883c01c2bc5151b7a976648f7b
Flori <florian.jaeger1@freenet.de>
2023-05-15 19:58:07 +0200

    Added a Map to the frontend

--- frontend-draft/src/app/app.component.ts
+++ frontend-draft/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+declare var ol: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   step = 0;
+
+  latitude: number = 52.531677;
+  longitude: number = 13.381777;
+
+  map: any;
+
 
   setStep(index: number) {
     this.step = index;
@@ -18,5 +27,52 @@ export class AppComponent {
 
   prevStep() {
     this.step--;
+  }
+
+  ngOnInit() {
+    var mousePositionControl = new ol.control.MousePosition({
+      coordinateFormat: ol.coordinate.createStringXY(4),
+      projection: 'EPSG:4326',
+      // comment the following two lines to have the mouse position
+      // be placed within the map.
+      className: 'custom-mouse-position',
+      target: document.getElementById('mouse-position'),
+      undefinedHTML: '&nbsp;'
+    });
+
+
+    this.map = new ol.Map({
+      target: 'map',
+      controls: ol.control.defaults({
+        attributionOptions: {
+          collapsible: false
+        }
+      }).extend([mousePositionControl]),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        zoom: 8
+      })
+    });
+
+    this.map.on('click', function (args: { coordinate: any; }) {
+      console.log(args.coordinate);
+      var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+      console.log(lonlat);
+      
+      var lon = lonlat[0];
+      var lat = lonlat[1];
+      alert(`lat: ${lat} long: ${lon}`);
+    });
+  }
+
+  setCenter() {
+    var view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(8);
   }
 }
