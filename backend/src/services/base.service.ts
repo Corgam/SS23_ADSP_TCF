889e4f93159cb892f222e3beb2977a2ae50b4cfa
guttenberger <alex.guttenberger50@gmail.com>
2023-05-16 19:14:46 +0200

    #31 add base service

---
+++ backend/src/services/base.service.ts
@@ -0,0 +1,60 @@
+import { Model, UpdateQuery } from "mongoose";
+import { Entity } from "../interfaces/common/entity.interface"
+import { CRUDService } from "../interfaces/common/crud_service.interface";
+
+import NotFoundError from "../errors/NotFoundError";
+
+export abstract class BaseService<T extends Entity,C,U> implements CRUDService<T, C, U> {
+  constructor(
+    private readonly model: Readonly<Model<T>>
+  ) {}
+
+  // Creates a single basic data file from provided JSON
+  async create(createEntity: C): Promise<void> {
+    // Create new basic data file
+    // Save the basic data file into DB
+    this.model.create(createEntity);
+    return;
+  };
+
+  // Deletes the single basic data file with given ID
+  async delete(id: string): Promise<void> {
+    const file = await this.model.findByIdAndRemove(id, { useFindAndModify: false }).catch(console.log);
+
+    if (!file) {
+      throw new NotFoundError();
+    }
+
+    return;
+  };
+
+  // Updates a single basic data file by ID
+  async update(id: string, updateParams: U): Promise<void> {
+    const file = await this.model.findByIdAndUpdate<U>(id, updateParams as UpdateQuery<T>, {
+      useFindAndModify: false,
+    });
+
+    if (!file) {
+      throw new NotFoundError();
+    }
+
+    return;
+  };
+
+  // Returns a single basic data file by ID
+  async get(id: string): Promise<T> {
+    const file = await this.model.findById(id);
+
+    if (!file) {
+      throw new NotFoundError();
+    }
+
+    return file;
+  };
+
+  // Returns all basic data files
+  async getAll(): Promise<T[]> {
+    return this.model.find();
+  };
+
+}
--- backend/src/services/basic_datafile.service.ts
+++ backend/src/services/basic_datafile.service.ts
@@ -1,62 +1,13 @@
-import { CRUDService } from "../interfaces/common/crud_service.interface";
-import BasicDataFileModel from "../models/basic_datafile.model";
 import {
   BasicDatafile,
   BasicDatafileCreateParams,
   BasicDatafileUpdateParams
 } from "../interfaces/entities/basic_datafile.entity";
-import NotFoundError from "../errors/NotFound.error";
+import basic_datafileModel from "../models/basic_datafile.model";
+import { BaseService } from "./base.service";
 
-export class BasicDatafileService implements CRUDService<BasicDatafile, BasicDatafileCreateParams, BasicDatafileUpdateParams> {
-  private readonly basicDataFileModel = BasicDataFileModel;
-
-  // Creates a single basic data file from provided JSON
-  async create(datafile: BasicDatafileCreateParams): Promise<void> {
-    // Create new basic data file
-    // Save the basic data file into DB
-    this.basicDataFileModel.create(datafile);
-
-    return;
-  };
-
-  // Deletes the single basic data file with given ID
-  async delete(id: string): Promise<void> {
-    const file = await this.basicDataFileModel.findByIdAndRemove(id, { useFindAndModify: false }).catch(console.log);
-
-    if (!file) {
-      throw new NotFoundError();
-    }
-
-    return;
-  };
-
-  // Updates a single basic data file by ID
-  async update(id: string, updateParams: BasicDatafileUpdateParams): Promise<void> {
-    const file = await this.basicDataFileModel.findByIdAndUpdate(id, updateParams, {
-      useFindAndModify: false,
-    });
-
-    if (!file) {
-      throw new NotFoundError();
-    }
-
-    return;
-  };
-
-  // Returns a single basic data file by ID
-  async get(id: string): Promise<BasicDatafile> {
-    const file = await this.basicDataFileModel.findById(id);
-
-    if (!file) {
-      throw new NotFoundError();
-    }
-
-    return file;
-  };
-
-  // Returns all basic data files
-  async getAll(): Promise<BasicDatafile[]> {
-    return this.basicDataFileModel.find();
-  };
-
+export default class BasicDatafileService extends BaseService<BasicDatafile, BasicDatafileCreateParams, BasicDatafileUpdateParams> { 
+  constructor() {
+    super(basic_datafileModel)
+  }
 }
