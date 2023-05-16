52d094cd3f5bdb4c00e20edd71e226ec62d49ebb
guttenberger <alex.guttenberger50@gmail.com>
2023-05-16 19:21:47 +0200

    #31 refactor: move id description to separate file

--- backend/src/controllers/basic_datafiles.controller.ts
+++ backend/src/controllers/basic_datafiles.controller.ts
@@ -1,87 +1,80 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Path,
-    Post,
-    Put,
-    Res,
-    Route,
-    Response,
-    SuccessResponse,
-    TsoaResponse,
-  } from "tsoa";
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Path,
+  Post,
+  Put,
+  Route,
+  Response,
+  SuccessResponse,
+} from "tsoa";
 
-  import { BasicDatafile, BasicDatafileCreateParams, BasicDatafileUpdateParams } from "../interfaces/entities/basic_datafile.entity";
-  import BasicDatafileService from "../services/basic_datafile.service";
-  import NotFoundError from "../errors/NotFoundError";
+import { BasicDatafile, BasicDatafileCreateParams, BasicDatafileUpdateParams } from "../interfaces/entities/basic_datafile.entity";
+import BasicDatafileService from "../services/basic_datafile.service";
+import NotFoundError from "../errors/NotFoundError";
+import { MongooseObjectId } from "../interfaces/common/MongooseObjectId";
+
+@Route("datafiles")
+export class BasicDatafileController extends Controller {
+  private readonly basicDatafileService = new BasicDatafileService();
 
   /**
-   * @pattern [0-9A-Fa-f]{24}
-   * @example "646365496740ded7a396f5d0"
+   * Retrieves the list of an existing files.
    */
-  export type MongoDBObjectId = string;
-
-  @Route("datafiles")
-  export class BasicDatafileController extends Controller {
-    private readonly basicDatafileService = new BasicDatafileService();
-
-    /**
-     * Retrieves the list of an existing files.
-     */
-    @Get()
-    public async getAllDataFiles(): Promise<BasicDatafile[]> {
-      return this.basicDatafileService.getAll();
-    }
-
-    /**
-     * Retrieves the details of an existing file.
-     * Supply the unique file ID and receive corresponding file details.
-     * @param fileId The file's identifier 
-     */
-    @Get("{fileId}")
-    @Response<NotFoundError>(404, "Not found")
-    public async getDatafile(
-      @Path() fileId: MongoDBObjectId,
-    ): Promise<BasicDatafile> {
-      return this.basicDatafileService.get(fileId);
-    }
-  
-    /**
-     * Creates a file.
-     */
-    @SuccessResponse("201", "Created") // Custom success response
-    @Post()
-    public async createDatafile(
-      @Body() body: BasicDatafileCreateParams
-    ): Promise<void> {
-      this.setStatus(201); // set return status 201
-      return this.basicDatafileService.create(body);
-    }
-
-    /**
-     * Deletes a file.
-     */
-    @Delete("{fileId}")
-    @Response<NotFoundError>(404, "Not found")
-    public async deleteDatafile(
-      @Path() fileId: MongoDBObjectId,
-    ): Promise<void> {
-      this.basicDatafileService.delete(fileId);
-      return;
-    }
-
-    /**
-     * Update a file.
-     */
-    @Put("{fileId}")
-    @Response<NotFoundError>(404, "Not found")
-    public async updateDatafile(
-      @Path() fileId: MongoDBObjectId,
-      @Body() body: BasicDatafileUpdateParams,
-    ): Promise<void> {
-      this.basicDatafileService.update(fileId, body);
-      return;
-    }
+  @Get()
+  public async getAllDataFiles(): Promise<BasicDatafile[]> {
+    return this.basicDatafileService.getAll();
   }
+
+  /**
+   * Retrieves the details of an existing file.
+   * Supply the unique file ID and receive corresponding file details.
+   * @param fileId The file's identifier 
+   */
+  @Get("{fileId}")
+  @Response<NotFoundError>(404, "Not found")
+  public async getDatafile(
+    @Path() fileId: MongooseObjectId,
+  ): Promise<BasicDatafile> {
+    return this.basicDatafileService.get(fileId);
+  }
+
+  /**
+   * Creates a file.
+   */
+  @SuccessResponse("201", "Created") // Custom success response
+  @Post()
+  public async createDatafile(
+    @Body() body: BasicDatafileCreateParams
+  ): Promise<void> {
+    this.setStatus(201); // set return status 201
+    return this.basicDatafileService.create(body);
+  }
+
+  /**
+   * Deletes a file.
+   */
+  @Delete("{fileId}")
+  @Response<NotFoundError>(404, "Not found")
+  public async deleteDatafile(
+    @Path() fileId: MongooseObjectId,
+  ): Promise<void> {
+    this.basicDatafileService.delete(fileId);
+    return;
+  }
+
+  /**
+   * Update a file.
+   */
+  @Put("{fileId}")
+  @Response<NotFoundError>(404, "Not found")
+  public async updateDatafile(
+    @Path() fileId: MongooseObjectId,
+    @Body() body: BasicDatafileUpdateParams,
+  ): Promise<void> {
+    this.basicDatafileService.update(fileId, body);
+    return;
+  }
+}
