45b011e59ff54bfede0bf427ea8250355a0a4ae8
Theo <105156462+thdrbrkw@users.noreply.github.com>
2023-05-17 11:20:21 +0200

    FE: Initial setup (#37)
    Co-authored-by: Flori <florian.jaeger1@freenet.de>
    Co-authored-by: Alexander Guttenberger <alex.guttenberger50@gmail.com>

---
+++ frontend-draft/src/app/app.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+
+
+declare var ol: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  step = 0;
+
+  latitude: number = 52.531677;
+  longitude: number = 13.381777;
+
+  map: any;
+
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+  ngOnInit() {
+    var mousePositionControl = new ol.control.MousePosition({
+      coordinateFormat: ol.coordinate.createStringXY(4),
+      projection: 'EPSG:4326',
+      // comment the following two lines to have the mouse position
+      // be placed within the map.
+      className: 'custom-mouse-position',
+      target: document.getElementById('mouse-position'),
+      undefinedHTML: '&nbsp;'
+    });
+
+
+    this.map = new ol.Map({
+      target: 'map',
+      controls: ol.control.defaults({
+        attributionOptions: {
+          collapsible: false
+        }
+      }).extend([mousePositionControl]),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        zoom: 8
+      })
+    });
+
+    this.map.on('click', function (args: { coordinate: any; }) {
+      console.log(args.coordinate);
+      var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+      console.log(lonlat);
+      
+      var lon = lonlat[0];
+      var lat = lonlat[1];
+      alert(`lat: ${lat} long: ${lon}`);
+    });
+  }
+
+  setCenter() {
+    let view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(8);
+  }
+}
