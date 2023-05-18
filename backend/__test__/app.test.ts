6ebac98849a4ef88c27ed9b71c62bc22d9661bed
Fr3574 <frederik.stalschus@live.de>
2023-05-18 15:36:04 +0200

    fix test but fail docker

--- backend/__test__/app.test.ts
+++ backend/__test__/app.test.ts
@@ -6,6 +6,6 @@ describe("GET /", () => {
   it("should return 'Hello World'", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Not what you expected");
+    expect(response.text).toBe("Hello World");
   });
 });
