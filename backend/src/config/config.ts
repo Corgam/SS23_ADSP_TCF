6b4076d81208173a4553ebcabb426527615940e3
Fr3574 <frederik.stalschus@live.de>
2023-05-20 14:28:34 +0200

    fix docker compose and optimize dockerfile

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,9 +2,9 @@ export default {
   // URL of the MongoDB database to connect to.
   // With Docker, the name of the container can also be provided instead of the IP address.
   // Here: mongodb://<IP>:<PORT>/<COLLECTION_NAME>
-  MONGODB_URL: "mongodb://localhost:27017/datastore",
+  MONGODB_URL: "mongodb://mongodb:27017/datastore",
   // Port of the express server
   PORT: process.env.PORT ? +process.env.PORT : 8080,
   // IP of the express server
-  HOST: "0.0.0.0",
+  HOST: "127.0.0.1",
 } as const;
