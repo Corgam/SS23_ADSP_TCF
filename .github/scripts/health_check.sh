caa8b8cfe145cfc4b0206cd52e7d6d66bc112e1b
Fr3574 <55927943+Fr3574@users.noreply.github.com>
2023-05-23 15:40:55 +0200

    chore: Add github workflows for ci and fix docker-compose (#53)
    Co-authored-by: Emil Balicki <emil.balicki2@gmail.com>

---
+++ .github/scripts/health_check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MAX_RETRIES=3
+SLEEP_SECONDS=5
+ROUTE=http://localhost:40000/health
+
+attempt=1
+while [[ $attempt -le $MAX_RETRIES ]]; do        
+    if [[ $(curl -s $ROUTE) == '{"status":"healthy"}' ]]; then
+        echo "Health check passed"
+        exit 0
+    fi
+    
+    echo "Health check failed, retrying in $SLEEP_SECONDS seconds..."
+    sleep $SLEEP_SECONDS
+    
+    attempt=$((attempt + 1))
+done
+
+echo "Health check failed after $MAX_RETRIES attempts"
+exit 1
