cec6deae3e92e8b87303af046f67c4efa8320df8
Emil Balicki <emil.balicki2@gmail.com>
2023-05-24 12:34:56 +0200

    Changed the log of IP

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,5 +6,5 @@ export default {
   // Port of the express server
   PORT: +(process.env.PORT ?? 8080),
   // IP of the express server
-  HOST: process.env.HOST ?? "0.0.0.0",
+  HOST: process.env.HOST ?? "localhost",
 } as const;
