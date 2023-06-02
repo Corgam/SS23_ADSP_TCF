69c7f2f441a9af16d6971d81d1d7a2acf1070d0d
flori950 <florian.jaeger1@freenet.de>
2023-06-02 13:34:02 +0200

    edit show icon

--- frontend/src/app/shared/header/top-menu.component.ts
+++ frontend/src/app/shared/header/top-menu.component.ts
@@ -33,7 +33,13 @@ export class TopMenuComponent implements OnInit {
   ngOnInit() {
     this.coordinateService.coordinate$.subscribe((coordinate) => {
       this.coordinate = coordinate;
-      // do something with the coordinate
+       // Check router
+  this.router.events.subscribe((event) => {
+    if (event instanceof NavigationEnd) {
+      this.showBackButton = event.url !== '/'; 
+      this.isFirstLoad = false;
+    }
+  });
     });
 
     this.translate.onLangChange.subscribe(() => {
@@ -49,6 +55,10 @@ export class TopMenuComponent implements OnInit {
         this.isFirstLoad = false;
       }
     });
+  }
+
+  shouldShowBackButton(): boolean {
+    return this.showBackButton && this.router.url !== '/' && this.router.url !== '/dashboard';
   }
 
   initializeHeader() {
