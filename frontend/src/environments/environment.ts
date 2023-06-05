f3a5c942e2500658a94b092b8da3efc7c3ae7bee
Simon Albani <simon@albani-mail.de>
2023-06-05 21:19:01 +0200

    fix(frontend):  remove credentials

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,12 +1,2 @@
 export const environment = {
-  firebase: {
-    apiKey: 'AIzaSyBm3rzxOPbqGfA9ss-nrxqxg30vwd7Gb80',
-    authDomain: 'zeebro-ottserver.firebaseapp.com',
-    databaseURL: 'https://zeebro-ottserver.firebaseio.com',
-    projectId: 'zeebro-ottserver',
-    storageBucket: 'zeebro-ottserver.appspot.com',
-    messagingSenderId: '410397722638',
-    appId: '1:410397722638:web:cb43167f98225bcca8145f',
-    measurementId: 'G-C2GJ0M5204',
-  },
 };
