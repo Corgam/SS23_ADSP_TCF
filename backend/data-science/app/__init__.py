19b2495a27d8fe1d15373e2648348dfff2c0ff0a
Alexander Guttenberger <alex.guttenberger50@gmail.com>
2023-06-29 12:11:37 +0200

    Add python microservice (#135)

---
+++ backend/data-science/app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_restx import Api
+
+def add_health_check_endpoint(app):
+    @app.route('/health')
+    def health_check():
+        return { "status": "healthy" }
+    
+def add_api_routes(app):
+    api = Api(
+        app,
+        version="1.0",
+        title="Python data science microservice",
+        description="Microservice for data science operations",
+        doc="/docs",
+        prefix="/api"
+    )
+
+    # Import the controllers
+    from app.controllers.data_processing_controller import api as data_processing_api
+
+    # Add namespaces to the API
+    api.add_namespace(data_processing_api)
+
+
+def create_app(mode, config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    add_health_check_endpoint(app)
+    add_api_routes(app)
+
+    return app
