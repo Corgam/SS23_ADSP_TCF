435448b289d43d68d7babaf6a8ae034cdeae006c
guttenberger <alex.guttenberger50@gmail.com>
2023-07-11 14:00:04 +0200

    add netcdf transformation to json

--- backend/data-science/app/controllers/data_processing_controller.py
+++ backend/data-science/app/controllers/data_processing_controller.py
@@ -1,11 +1,32 @@
-from flask import request
+from flask import request, Response, stream_with_context
 from flask_restx import Namespace, Resource
 from app.services import data_processing_service
+from werkzeug.datastructures import FileStorage
+from app.errors import FailedToParseError
 
-api = Namespace('data-processing', description='Data Processing operations')
+api = Namespace('convert-netcdf-to-json', description='Data Processing operations')
 
-@api.route('/convert-netcdf-to-json')
+upload_parser = api.parser()
+upload_parser.add_argument('file', type=FileStorage, location='files', required=True)
+
+@api.route('')
+@api.expect(upload_parser)
 class ConvertNetCDFToJSON(Resource):
-    def get(self):
-        processed_data = data_processing_service.hello_world()
-        return processed_data
+    @api.response(200, 'Success')
+    @api.response(400, 'Bad Request')
+    def post(self):
+        """
+        Uploads a NetCDF file and converts it to JSON.
+        """
+        args = upload_parser.parse_args()
+        netCDF4_file = args['file']
+
+        try:
+            # TODO: validate the file before sending response
+            json_generator = data_processing_service.convert_netcdf_to_json(netCDF4_file)
+            return Response(
+                stream_with_context(json_generator),
+                mimetype='application/json'
+            )
+        except Exception as e:
+            raise FailedToParseError(e)
