838d90d0bd409a4bd4940409acf9366d7f715963
Emil Balicki <emil.balicki2@gmail.com>
2023-07-14 14:50:56 +0200

    Added mesh loading

--- frontend/src/app/journey/threejs-view/threejs-view.component.ts
+++ frontend/src/app/journey/threejs-view/threejs-view.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 
 @Component({
   selector: 'app-threejs-view',
@@ -8,60 +10,90 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
   styleUrls: ['./threejs-view.component.scss'],
 })
 export class ThreeJSComponent {
+  // Basic Three.js components
   private scene: THREE.Scene;
   private camera: THREE.Camera;
   private renderer: THREE.WebGLRenderer;
-  private cityMeshes: THREE.Mesh[];
   private controls: OrbitControls;
-
+  // Window properties
   private windowWidth = 1920 / 2;
   private windowHeight = 902 / 2;
-
+  // Toggle parameters
   private renderingStopped = true;
+  private objectsLoaded = false;
 
   constructor() {
-    // Basic parts
+    // Scene
     this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xffffff);
+    // Add light
+    const light = new THREE.PointLight();
+    light.position.set(2.5, 7.5, 15);
+    this.scene.add(light);
+    // Camera
     this.camera = new THREE.PerspectiveCamera(
       75,
       this.windowWidth / this.windowHeight,
       0.1,
-      1000
+      1000000
     );
-    this.renderer = new THREE.WebGLRenderer();
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    // Meshes and textures
-    this.cityMeshes = [];
-    this.cityMeshes.push(
-      new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-      )
-    );
-  }
-
-  ngOnInit() {
-    // Set the renderer size
-    this.renderer.setSize(this.windowWidth, this.windowHeight);
-    // Add the city meshes
-    this.cityMeshes.forEach((mesh) => {
-      this.scene.add(mesh);
-    });
-
     this.camera.position.z = 5;
+    // Renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(this.windowWidth, this.windowHeight);
+    // Orbit controls
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.zoomSpeed = 10;
     this.controls.update();
-
-    this.renderer.render(this.scene, this.camera);
-
-    // this.animate();
-    console.log('Rendering!');
   }
 
+  /**
+   * Loads the renderer and starts the render() function.
+   */
   loadRenderer() {
+    if (!this.objectsLoaded) {
+      // Load city meshes
+      const objLoader = new OBJLoader();
+      const mtlLoader = new MTLLoader();
+      mtlLoader.load(
+        'assets/threejs-city-data/tile1/tile1.mtl',
+        (materials) => {
+          materials.preload();
+          const sc = this.scene;
+          objLoader.setMaterials(materials);
+          objLoader.load(
+            'assets/threejs-city-data/tile1/tile1.obj',
+            (group) => {
+              const mesh = <THREE.Mesh>group.children[0];
+              mesh.frustumCulled = false;
+              mesh.geometry.center();
+              console.log(mesh);
+              sc.add(mesh);
+              this.hideLoadingDiv();
+            }
+          );
+        }
+      );
+      this.objectsLoaded = true;
+    }
+    // Append renderer
     const container = document.querySelector('.threejs-renderer');
     container!.appendChild(this.renderer.domElement);
     this.renderingStopped = false;
     this.render();
+  }
+
+  /**
+   * Main render loop for the Three.js view
+   */
+  render() {
+    if (!this.renderingStopped) {
+      requestAnimationFrame(this.render.bind(this));
+    }
+    this.controls.update();
+    // Update the renderer
+    this.renderer.render(this.scene, this.camera);
   }
 
   /**
@@ -72,14 +104,14 @@ export class ThreeJSComponent {
   }
 
   /**
-   * Main render loop for the Three.js view
+   * Hide the loading text component
    */
-  render() {
-    console.log('Test!');
-    if (!this.renderingStopped) {
-      requestAnimationFrame(this.render.bind(this));
+  hideLoadingDiv() {
+    const loadingDiv = document.querySelector(
+      '.threejs-loading-text'
+    ) as HTMLElement;
+    if (loadingDiv) {
+      loadingDiv.style.display = 'none';
     }
-    // Update the renderer
-    this.renderer.render(this.scene, this.camera);
   }
 }
