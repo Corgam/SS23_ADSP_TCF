976b901978dd6c73a861de9ca49d3303bf23602f
Alexander Guttenberger <alex.guttenberger50@gmail.com>
2023-07-18 17:03:27 +0200

    Add support for netcdf (#166)
    Co-authored-by: Fr3574 <frederik.stalschus@live.de>
    Co-authored-by: Emil Balicki <emil.balicki2@gmail.com>

---
+++ backend/public-api/src/services/bucket.service.ts
@@ -0,0 +1,83 @@
+import { mongo, connections } from "mongoose";
+
+/**
+ * A service for managing file operations using GridFSBucket.
+ */
+export class BucketService {
+  protected readonly db = connections[0].db; // MongoDB database connection
+  protected readonly bucket: mongo.GridFSBucket = new mongo.GridFSBucket(
+    this.db
+  ); // GridFSBucket instance for file operations
+
+  /**
+   * Deletes files with a given name from the GridFSBucket.
+   * @param name - The name of the files to delete.
+   * @returns A Promise that resolves to true when all files are deleted successfully.
+   */
+  async deleteFilesByName(name: string): Promise<boolean> {
+    const fileList = await this.bucket.find({ filename: name }).toArray(); // Find files with a specific name
+
+    for (const file of fileList) {
+      await this.bucket.delete(file._id); // Delete each file by its ObjectId
+    }
+
+    return true; // Return true when all files are deleted
+  }
+
+  /**
+   * Uploads a file to the GridFSBucket.
+   * If a file with the same name already exists, it is deleted before uploading the new file.
+   * @param filename - The name of the file to upload.
+   * @param file - The file content to upload.
+   * @param contentType - The MIME type of the file.
+   * @returns A Promise that resolves to the ObjectId of the uploaded file, or undefined if the upload fails.
+   */
+  async uploadFile(
+    filename: string,
+    file: unknown,
+    contentType?: string
+  ): Promise<string | undefined> {
+    await this.deleteFilesByName(filename); // Delete existing files with the same name
+
+    return new Promise((resolve, reject) => {
+      const uploadStream = this.bucket.openUploadStream(filename, {
+        contentType,
+      }); // Create an upload stream for the new file
+      uploadStream.write(JSON.stringify(file), "utf8"); // Write the file content to the upload stream
+      uploadStream.end((error, result) => {
+        if (error) {
+          reject(error); // Reject the Promise if an error occurs during the upload
+        } else {
+          if (result) {
+            resolve(String(result._id as unknown)); // Resolve the Promise with the ObjectId of the uploaded file
+          } else {
+            resolve(undefined); // Resolve the Promise with undefined if the upload result is missing
+          }
+        }
+      });
+    });
+  }
+
+  /**
+   * Downloads a file from the GridFSBucket by its ObjectId.
+   * @param id - The ObjectId of the file to download.
+   * @returns A Promise that resolves to the parsed content of the downloaded file.
+   */
+  downloadFile(id: string): Promise<unknown> {
+    return new Promise((resolve, reject) => {
+      const downloadStream = this.bucket.openDownloadStream(
+        id as unknown as mongo.ObjectId
+      ); // Create a download stream for the file
+      let file = "";
+      downloadStream.on("data", (chunk) => {
+        file += chunk.toString(); // Concatenate the chunks of data into a string
+      });
+      downloadStream.on("error", (error) => {
+        reject(error); // Reject the Promise if an error occurs during the download
+      });
+      downloadStream.on("end", () => {
+        resolve(JSON.parse(file)); // Resolve the Promise with the parsed content of the downloaded file
+      });
+    });
+  }
+}
---
+++ backend/public-api/src/services/datafile/datafileCERV2.service.ts
@@ -0,0 +1,287 @@
+import { v4 as uuidv4 } from "uuid";
+
+import {
+  SupportedDatasetFileTypes,
+  NotRefDataFile,
+  DataType,
+} from "../../../../../common/types";
+import datafileModel from "../../models/datafile.model";
+import {
+  handleNetCDFFileData,
+  handleNetCDFFileDataWithOptions,
+} from "./datafileRawParsing.service";
+
+type CoordinateMap = Map<any, any>;
+
+export async function handleCERV2File(
+  file: Express.Multer.File,
+  tags = "",
+  stepSize = 10,
+  description?: string
+) {
+  const uploadId = uuidv4();
+
+  const metadata = await handleNetCDFFileData(file, "/metadata");
+
+  const { locationWithTimeVariableNames, locationVariableNames } =
+    getVariablesNamesWithLocationData(metadata);
+
+  const tagList = [
+    "CERv2",
+    ...locationWithTimeVariableNames,
+    ...Array.from(tags.split(","), (tag) => tag.trim()),
+  ];
+
+  console.log("Adding data to data files");
+
+  const timeVarsMap = await createAndSaveLocationWithTimeVars(
+    file,
+    metadata,
+    locationWithTimeVariableNames,
+    uploadId,
+    stepSize
+  );
+
+  console.log("Finished timeVarsMap...");
+
+  const varsMap = await createAndSaveLocationVars(
+    file,
+    metadata,
+    locationVariableNames,
+    stepSize
+  );
+
+  console.log("Finished varsMap...");
+
+  for (const datafile of tansformMetadataToDatafile(
+    metadata,
+    file,
+    uploadId,
+    tagList,
+    timeVarsMap,
+    varsMap,
+    stepSize,
+    description
+  )) {
+    await datafileModel.create(datafile);
+  }
+
+  console.log("Finished creating datafiles.");
+}
+
+async function createAndSaveLocationVars(
+  file: Express.Multer.File,
+  metadata: any,
+  locationVariableNames: string[],
+  stepSize: number
+) {
+  // create content.data.vars
+  const locationVarMap = new Map();
+  const cerv2_var_gen = handleNetCDFFileDataWithOptions(file, "/data", {
+    filter: locationVariableNames,
+    isCERv2: true,
+    stepSize,
+  });
+
+  for await (const cerv2_var of cerv2_var_gen) {
+    addValuesToLocationMap(locationVarMap, metadata, cerv2_var, stepSize);
+  }
+
+  return locationVarMap;
+}
+
+async function createAndSaveLocationWithTimeVars(
+  file: Express.Multer.File,
+  metadata: any,
+  locationWithTimeVariableNames: string[],
+  uploadId: string,
+  stepSize: number
+) {
+  const locationTimeVarMap = new Map();
+  const cerv2_var_gen = handleNetCDFFileDataWithOptions(file, "/data", {
+    filter: locationWithTimeVariableNames,
+    isCERv2: true,
+    stepSize,
+  });
+
+  for await (const cerv2_var of cerv2_var_gen) {
+    addValuesToTimeLocationMap(
+      locationTimeVarMap,
+      metadata,
+      cerv2_var,
+      stepSize
+    );
+  }
+
+  return locationTimeVarMap;
+}
+
+function* tansformMetadataToDatafile(
+  metadata: any,
+  file: Express.Multer.File,
+  uploadId: string,
+  tags: string[],
+  timeVarsMap: Map<any, any>,
+  varsMap: Map<any, any>,
+  stepSize: number,
+  description?: string
+): Generator<NotRefDataFile> {
+  let dataId = 0;
+
+  const xCoordinateOffset = +metadata["global_attributes"]["PROJ_CENTRAL_LON"];
+  const yCoordinateOffset = +metadata["global_attributes"]["PROJ_CENTRAL_LAT"];
+
+  const x_count = Math.ceil(metadata["dimensions"]["west_east"] / stepSize);
+  const y_count = Math.ceil(metadata["dimensions"]["south_north"] / stepSize);
+
+  for (let x = 0; x < x_count; x++) {
+    for (let y = 0; y < y_count; y++) {
+      const key = JSON.stringify({
+        x: xCoordinateOffset + x + stepSize,
+        y: yCoordinateOffset + y + stepSize,
+      });
+
+      const { lat, lon } = varsMap.get(key);
+
+      description ??= `A datapoint no.${dataId} from CERV2 dataset file: ${file.originalname}`;
+
+      const datafile: NotRefDataFile = {
+        title: `${file.originalname}_${dataId}`,
+        description: description,
+        dataType: DataType.NOTREFERENCED,
+        dataSet: SupportedDatasetFileTypes.CERV2,
+        tags,
+        traceId: uploadId,
+        content: {
+          data: {
+            netCDFInfo: metadata,
+            timeVars: timeVarsMap.get(key),
+          },
+          location: {
+            type: "Point",
+            coordinates: [lon, lat],
+          },
+        },
+      };
+      dataId++;
+      yield datafile;
+    }
+  }
+}
+
+export function getVariablesNamesWithLocationData(metadata: any) {
+  // get variable names
+  const locationWithTimeVariableNames = [];
+  const locationVariableNames = [];
+
+  for (const [variable_name, variable_value] of Object.entries<any>(
+    metadata["variables_metadata"]
+  )) {
+    const hasTimeDimension = variable_value["dimensions"].includes("time");
+    const hasXDimension = variable_value["dimensions"].includes("west_east");
+    const hasYDimension = variable_value["dimensions"].includes("south_north");
+
+    if (hasTimeDimension && hasXDimension && hasYDimension) {
+      locationWithTimeVariableNames.push(variable_name);
+    }
+    if (!hasTimeDimension && hasXDimension && hasYDimension) {
+      locationVariableNames.push(variable_name);
+    }
+  }
+
+  return { locationWithTimeVariableNames, locationVariableNames };
+}
+
+export function addValuesToTimeLocationMap(
+  locationTimeVarMap: Map<string, any>,
+  metadata: any,
+  data: any,
+  stepSize: number
+): CoordinateMap {
+  const xCoordinateOffset = +metadata["global_attributes"]["PROJ_CENTRAL_LON"];
+  const yCoordinateOffset = +metadata["global_attributes"]["PROJ_CENTRAL_LAT"];
+  const x_count = Math.ceil(metadata["dimensions"]["west_east"] / stepSize);
+  const y_count = Math.ceil(metadata["dimensions"]["south_north"] / stepSize);
+  const time_count = metadata["dimensions"]["time"];
+
+  const variable = Object.keys(data["variables_data"])[0];
+
+  for (let x = 0; x < x_count; x++) {
+    for (let y = 0; y < y_count; y++) {
+      for (let t = 0; t < time_count; t++) {
+        // append if already exists
+        const key = JSON.stringify({
+          x: xCoordinateOffset + x + stepSize,
+          y: yCoordinateOffset + y + stepSize,
+        });
+        let current = locationTimeVarMap.get(key) ?? {};
+        const value = data["variables_data"][variable]["data"][x][y][t];
+
+        if (Array.isArray(value)) {
+          const dimensions =
+            data["variables_data"][variable]["dimensions"].slice(3);
+          current = {
+            ...current,
+            [variable]: value,
+            dimensions,
+          };
+        } else {
+          current = {
+            ...current,
+            [variable]: value,
+          };
+        }
+
+        locationTimeVarMap.set(key, current);
+      }
+    }
+  }
+
+  return locationTimeVarMap;
+}
+
+export function addValuesToLocationMap(
+  locationVarMap: Map<string, any>,
+  metadata: any,
+  data: any,
+  stepSize: number
+): CoordinateMap {
+  const xCoordinateOffset = +metadata["global_attributes"]["PROJ_CENTRAL_LON"];
+  const yCoordinateOffset = +metadata["global_attributes"]["PROJ_CENTRAL_LAT"];
+  const x_count = Math.ceil(metadata["dimensions"]["west_east"] / stepSize);
+  const y_count = Math.ceil(metadata["dimensions"]["south_north"] / stepSize);
+
+  const variable = Object.keys(data["variables_data"])[0];
+
+  // get variable data for each location variable
+  for (let x = 0; x < x_count; x++) {
+    for (let y = 0; y < y_count; y++) {
+      // append if already exists
+      const key = JSON.stringify({
+        x: xCoordinateOffset + x + stepSize,
+        y: yCoordinateOffset + y + stepSize,
+      });
+      let current = locationVarMap.get(key) ?? {};
+      const value = data["variables_data"][variable]["data"][x][y];
+
+      if (Array.isArray(value)) {
+        const dimensions =
+          data["variables_data"][variable]["dimensions"].slice(2);
+        current = {
+          ...current,
+          [variable]: value,
+          dimensions,
+        };
+      } else {
+        current = {
+          ...current,
+          [variable]: value,
+        };
+      }
+
+      locationVarMap.set(key, current);
+    }
+  }
+
+  return locationVarMap;
+}
