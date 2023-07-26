57833fbd6630fbfa3177318b11689411e9dbb987
Emil Balicki <emil.balicki2@gmail.com>
2023-07-26 18:15:20 +0200

    Fixed querying by ID

--- backend/public-api/src/services/filter/stringFilter.service.ts
+++ backend/public-api/src/services/filter/stringFilter.service.ts
@@ -1,14 +1,24 @@
 import { StringFilter } from "../../../../../common/types";
 import { JsonObject } from "swagger-ui-express";
+import { ObjectId } from "mongodb";
 
 // Handles the CONTAINS filter operation
 export function createFilterQueryContains(filter: StringFilter): JsonObject {
   const keyString = filter.key;
   // Create the conditional
   let query: JsonObject = { $regex: filter.value, $options: "i" };
-  // Append the NOT operation
-  if (filter.negate) {
-    query = { $not: query };
+  // Check if ID is passed
+  if (filter.key === "_id") {
+    query = new ObjectId(filter.value);
+    // Append the NOT operation
+    if (filter.negate) {
+      query = { $ne: query };
+    }
+  } else {
+    // Append the NOT operation
+    if (filter.negate) {
+      query = { $not: query };
+    }
   }
   // Return the final json query
   return {
@@ -21,6 +31,10 @@ export function createFilterQueryMatches(filter: StringFilter): JsonObject {
   const keyString = filter.key;
   // Create the conditional
   let query: JsonObject | string = filter.value;
+  // Check if ID is passed
+  if (filter.key === "_id") {
+    query = new ObjectId(filter.value);
+  }
   // Append the NOT operation
   if (filter.negate) {
     query = { $ne: query };
