29f64598dafd7a495d4e71501b5966af646eb838
thdrbrkw <theodor.barkow@campus.tu-berlin.de>
2023-08-12 16:19:48 +0200

    Refactored module

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { DashboardComponent } from "./dashboard.component";
-import { DashboardTileComponent } from "./dashboard-tile/dashboard-tile.component";
 import { MaterialModule } from "../material.module";
 import { CommonModule } from "@angular/common";
 import { DashboardRoutingModule } from "./dashboard.routing-module";
@@ -16,8 +15,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 
 @NgModule({
   declarations: [
-    DashboardComponent,
-    DashboardTileComponent
+    DashboardComponent
   ],
   bootstrap: [],
   imports: [
