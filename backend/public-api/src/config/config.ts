2d1e11c576e7f1a7e0421803a7ec490dd1c204fa
Emil Balicki <emil.balicki2@gmail.com>
2023-08-17 16:56:36 +0200

    Changed some ports

--- backend/public-api/src/config/config.ts
+++ backend/public-api/src/config/config.ts
@@ -6,7 +6,7 @@ export default {
   // Here: mongodb://<IP>:<PORT>/<COLLECTION_NAME>
   MONGODB_URL: process.env.MONGODB_URL ?? "mongodb://localhost:27017/datastore",
   // Port of the express server
-  PORT: +(process.env.PORT ?? 8080),
+  PORT: process.env.PORT ?? 40000,
   // IP of the express server
   HOST: process.env.HOST ?? "localhost",
   // Disabled swagger authentication,
@@ -14,7 +14,7 @@ export default {
   // URL of the data science server
   DATASCIENCE_BASE_URL: `http://${
     process.env.PYTHON_BACKEND_HOST ?? "localhost"
-  }:${process.env.PYTHON_BACKEND_PORT ?? 8800}/api`,
+  }:${process.env.PYTHON_BACKEND_PORT ?? 50000}/api`,
   // firebase config
   FIREBASE_CONFIG: frontendEnvironment.firebase,
 } as const;
