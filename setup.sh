d0514bdd332d43ccb4f6267484550904dc5e81a1
guttenberger <alex.guttenberger50@gmail.com>
2023-08-23 14:50:00 +0200

    fix python installation

--- setup.sh
+++ setup.sh
@@ -4,9 +4,8 @@
 # Install [Docker](https://docs.docker.com/engine/install/), [Docker Compose](https://docs.docker.com/compose/),
 # and run the Docker Daemon (for Ubuntu follow the [Docker Installation Guide](https://docs.docker.com/engine/install/ubuntu/).
 echo "[TCF Setup] Installing Docker..."
-{
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -15,12 +14,10 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-} > /dev/null 2>&1
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 # Install all necessary apt packages
 echo "[TCF Setup] Installing necessary apt packages..."
-{
-sudo apt install python3.8 -y
+sudo apt install python3.8 python3-pip -y
 python3.8 -m pip install --upgrade pip
 python3.8 -m pip install pipenv
 # Update Node to >= 16.0.0 ([Ubuntu Guide](https://github.com/nodesource/distributions))
@@ -28,9 +25,7 @@ echo "[TCF Setup] Updating NodeJS..."
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 # Run the npm setup for our project
-} > /dev/null 2>&1
 echo "[TCF Setup] Running project's setup..."
-{
 npm run setup
 
 ### MONGODB SCRIPTS ###
@@ -38,7 +33,6 @@ npm run setup
 # Install pip packages `pip install pymongo faker`:
 # - [PyMongo](https://www.mongodb.com/docs/drivers/pymongo/) this library provides a Python interface for connecting to and interacting with MongoDB.
 # - [Faker](https://github.com/joke2k/faker) is a Python library used for generating fake data, such as names, addresses, phone numbers, and more. It is utilized in this script to create realistic-looking data.
-} > /dev/null 2>&1
 echo "[TCF Setup] Installing packages for MongoDB scripts..."
 sudo python3.8 -m pip install pymongo faker
 
@@ -52,7 +46,7 @@ else
     echo "User '$USER' has been added to the 'docker' group."
 fi
 
-echo "[TCF Setup] Deploy application ..."
-newgrp docker << EOF
-  npm run dev:all
-EOF
+newgrp docker
+
+echo "[TCF Setup] Setup complete!"
+echo "Please restart your terminal for the changes to take effect."
